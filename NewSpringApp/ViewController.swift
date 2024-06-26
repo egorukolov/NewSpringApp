d70897a1667c508f861f1a842431e67779b1827a
Egor Ukolov <ukolovegor@yandex.ru>
2024-06-26 13:17:19 +0700

    Initial Commit

---
+++ NewSpringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewSpringApp
+//
+//  Created by Egor Ukolov on 26.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
