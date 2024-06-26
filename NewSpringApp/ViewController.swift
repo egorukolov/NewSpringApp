e8668496013819a1e7b094cb5fbafc92fcf0b1d5
Egor Ukolov <ukolovegor@yandex.ru>
2024-06-26 20:55:52 +0700

    Начальная настройка анимации View (стартовая и по нажатию кнопки)

--- NewSpringApp/ViewController.swift
+++ NewSpringApp/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Egor Ukolov on 26.06.2024.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var parametrsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+        
+        springAnimationView.animation = "flash"
+        springAnimationView.curve = "easeIn"
+        springAnimationView.delay = 0.5
+        springAnimationView.duration = 0.5
+        springAnimationView.force = 2
+        springAnimationView.animate()
+        
+        sender.animation = "swing"
+        sender.delay = 0.2
+        sender.duration = 0.3
+        sender.force = 1
+        sender.animate()
+    }
+    
+    
 }
 
